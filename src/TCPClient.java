6186cff8f02f73793ac81c2b6ce3785d2b0f8e54
HaJaehee <lovesm135@naver.com>
2017-02-14 23:46:48 +0900

    commit

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,33 +1,40 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.Charset;
  
  
 public class TCPClient {
  
 	public static void main(String[] args) {
  
+		int serverPort = 10001;
+		Socket sock;
+		
 		try {
-			Socket socket = new Socket("localhost", 10000);
+			sock = new Socket("localhost", serverPort);
  
 			BufferedReader in = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
+					sock.getInputStream(),Charset.forName("UTF-8")));
 
-			OutputStream out = socket.getOutputStream();
- 
-			out.write("Hello Java Tcp Client!!!! \n".getBytes());
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+					sock.getOutputStream(),Charset.forName("UTF-8")));
+
+			out.write("Hello Java TCP Server!!!! \n");
 			out.flush();
  
-			String line = in.readLine();
-			System.out.println("Reply : "+line);
+			String inputLine = in.readLine();
+			System.out.println("Data : " + inputLine);
  
 			in.close();
 			out.close();
-			socket.close();
+			sock.close();
  
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
